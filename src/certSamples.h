c36a89a6dd6f10207390eaf452e9b7e8c277e56f
Anthony-Mai <Anthony_Mai_2000@hotmail.com>
2015-02-10 16:11:57 -0800

    Create certSamples.h

---
+++ src/certSamples.h
@@ -0,0 +1,24 @@
+#ifndef _CERTSAMPLES_H_INCLUDED_6_29_2014_
+#define _CERTSAMPLES_H_INCLUDED_6_29_2014_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+
+extern const uchar gGeoTrustRoot[856];
+extern const uchar gGoogleCA[1032];
+extern const uchar gGoogle[1146];
+
+extern const uchar gMySSLRootCert[980];
+extern const uchar gMySSLPublicKey[256];
+extern const uchar gMySSLPrivateKey[256];
+extern const uchar gMySSLCert[961];
+
+
+#ifdef __cplusplus
+} //extern "C"
+#endif //__cplusplus
+
+
+#endif //#ifndef _CERTSAMPLES_H_INCLUDED_6_29_2014_
