fe5274894a5b6322535803f242b8cfabd756c65b
Anthony-Mai <Anthony_Mai_2000@hotmail.com>
2015-02-10 16:23:45 -0800

    Create sslTest.h

---
+++ src/sslTest.h
@@ -0,0 +1,17 @@
+#ifndef _SSLTEST_H_INCLUDED_6_28_2014_
+#define _SSLTEST_H_INCLUDED_6_28_2014_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+
+uint    DoSSLTest();
+
+#ifdef __cplusplus
+} //extern "C"
+#endif //__cplusplus
+
+
+#endif //#ifndef _SSLTEST_H_INCLUDED_6_28_2014_
